d9ea9a836b56e5eecdaec02696b07a8624a0f6f4
Romina <roma.marcoleta22@gmail.com>
2025-08-22 10:17:37 -0400

    arreglo de app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,116 +27,31 @@ export default function App() {
     <NotificationProvider>
       <AuthProvider>
         <CartProvider>
-        <CotizacionesProvider>
-          <Router>
-            <ProtectedRoute>
-              <NavbarResponsive />
+          <CotizacionesProvider>
+            <Router>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route 
-                  path="/users" 
-                  element={
-                    <ProtectedRoute requiredRole="admin">
-                      <Users />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/projects" 
-                  element={
-                    <ProtectedRoute requiredPermission="proyectos">
-                      <Projects />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/projects/:id/materiales" 
-                  element={
-                    <ProtectedRoute requiredPermission="proyectos">
-                      <ProjectMaterials />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/providers" 
-                  element={
-                    <ProtectedRoute requiredPermission="proveedores">
-                      <Providers />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/insumos" 
-                  element={
-                    <ProtectedRoute requiredPermission="materiales">
-                      <Insumos />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/cotizaciones" 
-                  element={
-                    <ProtectedRoute requiredPermission="cotizaciones">
-                      <Router>
-                        <Routes>
-                          {/* Página pública sin Navbar ni ProtectedRoute */}
-                          <Route path="/corporacion-tarapaka" element={<CorporacionTarapaka />} />
-                          {/* Resto de rutas protegidas y privadas */}
-                          <Route path="/" element={<Home />} />
-                          <Route 
-                            path="/users" 
-                            element={
-                              <ProtectedRoute requiredRole="admin">
-                                <Users />
-                              </ProtectedRoute>
-                            }
-                          />
-                          <Route 
-                            path="/projects" 
-                            element={
-                              <ProtectedRoute requiredPermission="proyectos">
-                                <Projects />
-                              </ProtectedRoute>
-                            }
-                          />
-                          <Route 
-                            path="/projects/:id/materiales" 
-                            element={<ProjectMaterials />} 
-                          />
-                          <Route path="/providers" element={<Providers />} />
-                          <Route path="/insumos" element={<Insumos />} />
-                          <Route path="/cotizaciones" element={<Cotizaciones />} />
-                          <Route path="/actas-reunion" element={<ActasReunion />} />
-                          <Route path="/buscador" element={<BuscadorPage />} />
-                          <Route path="/historial-cotizaciones" element={<HistorialCotizaciones />} />
-                          <Route path="/configuracion" element={<ConfiguracionPage />} />
-                          <Route path="/excel-online" element={<ExcelOnline />} />
-                          <Route path="/presupuestos" element={<Presupuestos />} />
-                          <Route path="/cotizacion-cart-v2" element={<CotizacionCartV2 />} />
-                          <Route path="/cart-button" element={<CartButton />} />
-                        </Routes>
-                      </Router>
-                  element={
-                    <ProtectedRoute requiredPermission="proyectos">
-                      <Presupuestos />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/excel" 
-                  element={
-                    <ProtectedRoute>
-                      <ExcelOnline />
-                    </ProtectedRoute>
-                  } 
-                />
+                {/* Página pública sin Navbar ni ProtectedRoute */}
+                <Route path="/corporacion-tarapaka" element={<CorporacionTarapaka />} />
+                {/* Rutas privadas */}
+                <Route path="/" element={<ProtectedRoute><NavbarResponsive /><Home /></ProtectedRoute>} />
+                <Route path="/users" element={<ProtectedRoute requiredRole="admin"><NavbarResponsive /><Users /></ProtectedRoute>} />
+                <Route path="/projects" element={<ProtectedRoute requiredPermission="proyectos"><NavbarResponsive /><Projects /></ProtectedRoute>} />
+                <Route path="/projects/:id/materiales" element={<ProtectedRoute requiredPermission="proyectos"><NavbarResponsive /><ProjectMaterials /></ProtectedRoute>} />
+                <Route path="/providers" element={<ProtectedRoute requiredPermission="proveedores"><NavbarResponsive /><Providers /></ProtectedRoute>} />
+                <Route path="/insumos" element={<ProtectedRoute requiredPermission="materiales"><NavbarResponsive /><Insumos /></ProtectedRoute>} />
+                <Route path="/cotizaciones" element={<ProtectedRoute requiredPermission="cotizaciones"><NavbarResponsive /><Cotizaciones /></ProtectedRoute>} />
+                <Route path="/actas-reunion" element={<ProtectedRoute><NavbarResponsive /><ActasReunion /></ProtectedRoute>} />
+                <Route path="/buscador" element={<ProtectedRoute><NavbarResponsive /><BuscadorPage /></ProtectedRoute>} />
+                <Route path="/historial-cotizaciones" element={<ProtectedRoute><NavbarResponsive /><HistorialCotizaciones /></ProtectedRoute>} />
+                <Route path="/configuracion" element={<ProtectedRoute><NavbarResponsive /><ConfiguracionPage /></ProtectedRoute>} />
+                <Route path="/excel-online" element={<ProtectedRoute><NavbarResponsive /><ExcelOnline /></ProtectedRoute>} />
+                <Route path="/presupuestos" element={<ProtectedRoute requiredPermission="proyectos"><NavbarResponsive /><Presupuestos /></ProtectedRoute>} />
+                <Route path="/cotizacion-cart-v2" element={<ProtectedRoute><NavbarResponsive /><CotizacionCartV2 /></ProtectedRoute>} />
+                <Route path="/cart-button" element={<ProtectedRoute><NavbarResponsive /><CartButton /></ProtectedRoute>} />
               </Routes>
-              <CotizacionCartV2 />
-              <CartButton />
-            </ProtectedRoute>
-          </Router>
-        </CotizacionesProvider>
-      </CartProvider>
+            </Router>
+          </CotizacionesProvider>
+        </CartProvider>
       </AuthProvider>
     </NotificationProvider>
   );
